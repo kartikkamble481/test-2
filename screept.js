61fef9843b374b13d562959b79942f7b3ebab658
kartikkamble481 <kartikkamble481@gmail.com>
2026-07-31 19:57:08 +0530

    add new file & code changes

---
+++ screept.js
@@ -0,0 +1,9 @@
+function changeMessage() {
+
+    let message = document.getElementById("message");
+
+    message.innerHTML = "🎉 JavaScript is Working Successfully!";
+
+    message.style.color = "blue";
+    message.style.fontWeight = "bold";
+}
